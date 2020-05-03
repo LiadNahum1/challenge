aba20b7fc5903e38f88bbe92333724a213af4172
LiadNahum1 <liadna@post.bgu.ac.il>
2020-05-03 17:20:24 +0300

    add features 2

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,18 +4,50 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 
-USER_COUNT = 40
-WORDS_COUNT_PER_SEGMENT = 100
+USER_COUNT = 4
+WORDS_COUNT_PER_SEGMENT = 10
 SEGMENT_COUNT = 150
 TRAIN_SEGMENT_COUNT = 50
 
+# returns a dictinary of the words in dict with the occurance of them in specific segment
+def count_word_occurrence(dict, segment):
+    for word in segment:
+        if word in dict:
+            dict[word] = dict[word] + 1
+    return dict
 
+# feature 1 - commands in cmds
+# returns a dictionary of all commands with zero
+def build_word_dict_feature_1():
+    commends_dict = dict()
+    file_of_commends = open("cmds", "r")
+    for line in file_of_commends:
+        commends_dict[line[:-1]] = 0
+    return commends_dict
+
+# returns an array with the number of occurrence of each word in each segment of specific user
+def separate_user_to_segment_feature_1(user_id):
+    user_segments = []
+    file_of_user = open("FraudedRawData/User" + str(user_id), "r")
+    for i in range(0, SEGMENT_COUNT):
+        lines = []
+        for j in range(0, WORDS_COUNT_PER_SEGMENT):
+            lines.append(file_of_user.readline()[:-1])
+        dict = build_word_dict_feature_1()
+        segment_words_count = list(count_word_occurrence(dict, lines).values())
+        user_segments.append(segment_words_count)
+    return user_segments
+
+
+# feature 2 - 3grams of commands taking the best 1000 according to tidf
+
+# returns 3grams commands tf-idf for each user
 def tidf_n_grams():
     words_per_user = []
     for i in range(0, USER_COUNT):
         file_of_user = open("FraudedRawData/User" + str(i), "r")
         n_gram_str = ""
-        for j in range(0, WORDS_COUNT_PER_SEGMENT * SEGMENT_COUNT):
+        for j in range(0, WORDS_COUNT_PER_SEGMENT * TRAIN_SEGMENT_COUNT):
             n_gram = file_of_user.readline()[:-1]
             n_gram = n_gram + file_of_user.readline()[:-1]
             n_gram = n_gram + file_of_user.readline()[:-1]
@@ -28,60 +60,72 @@ def tidf_n_grams():
     return pd.DataFrame(denselist, columns=vectorizer.get_feature_names())
 
 
+# taking top 1000 ngrams for specific user
 def best_ngrams(user_id, n_grams_tidf):
-    fituer_number = 1000
-    print(n_grams_tidf.iloc[user_id:, ])
+    feature_number = 1000
+    columns = []
+    for i in range(0, USER_COUNT):
+        columns.append(f'user_{i}')
+    n_grams_tidf_T = pd.DataFrame(data=n_grams_tidf.T)
+    n_grams_tidf_T.columns = columns
+    best_ngrams = n_grams_tidf_T.nlargest(feature_number, f'user_{user_id}').index.to_list()
+    return best_ngrams
 
-    #user_top_n_grams = user_n_grams[:fituer_number]
-    #print(user_top_n_grams)
+# returns a dictionary of best ngrams with zero
+def build_word_dict_feature_2(best_ngrams):
+    ngrams_dict = dict()
+    for ngram in best_ngrams:
+        ngrams_dict[ngram] = 0
+    return ngrams_dict
 
-
-# returns an array with the number of occurrence of each word in each segment
-def separate_user_to_segment(user_id):
+# returns an array with the number of occurrence of each word in each segment of specific user
+def separate_user_to_segment_feature_2(user_id, n_grams_tidf):
     user_segments = []
     file_of_user = open("FraudedRawData/User" + str(user_id), "r")
+    top_ngrams = best_ngrams(user_id, n_grams_tidf)
     for i in range(0, SEGMENT_COUNT):
         lines = []
         for j in range(0, WORDS_COUNT_PER_SEGMENT):
-            lines.append(file_of_user.readline()[:-1])
-        segment_words_count = list(count_word_occurrence(lines).values())
+            n_gram = file_of_user.readline()[:-1]
+            n_gram = n_gram + file_of_user.readline()[:-1]
+            n_gram = n_gram + file_of_user.readline()[:-1]
+            lines.append(n_gram)
+        dict = build_word_dict_feature_2(top_ngrams)
+        segment_words_count = list(count_word_occurrence(dict, lines).values())
         user_segments.append(segment_words_count)
     return user_segments
 
 
-def build_word_dict():
-    commends_dict = dict()
-    file_of_commends = open("cmds", "r")
-    for line in file_of_commends:
-        commends_dict[line[:-1]] = 0
-    return commends_dict
 
 
-def count_word_occurrence(segment):
-    d = build_word_dict()
-    for word in segment:
-        if word in d:
-            d[word] = d[word] + 1
-    return d
+def get_all_features_of_all_users():
+    all_features_of_all_users = []
+    for user_id in range(0, USER_COUNT):
+        features_1 = separate_user_to_segment_feature_1(user_id)
+        features_2 = separate_user_to_segment_feature_2(user_id, tidf_grams)
+        all_features = []
+        for i in range(0, SEGMENT_COUNT):
+            all_features.append(features_1[i].extend(features_2[i]))
+        all_features_of_all_users.append(all_features)
+    return all_features_of_all_users
 
-
-def build_train_set(user_id):
-    train_set = separate_user_to_segment(user_id)[:TRAIN_SEGMENT_COUNT]
+def build_train_set(user_id, all_features_of_all_users):
+    segments_of_other_users = 2
+    all_features_of_id = all_features_of_all_users[user_id]
+    train_set = all_features_of_id[:TRAIN_SEGMENT_COUNT]
     train_labels = list(np.zeros(TRAIN_SEGMENT_COUNT))
     for other_user in range(0, USER_COUNT):
         if other_user != user_id:
-            train_set.append(separate_user_to_segment(other_user)[0])
-            train_labels.append(1)
+            train_set.append(all_features_of_all_users[other_user][:segments_of_other_users]) #taking two segemnts from each other user
+            train_labels.append(list(np.ones(segments_of_other_users)))
     return train_set, train_labels
 
-
-def build_test_set(user_id):
-    return separate_user_to_segment(user_id)[TRAIN_SEGMENT_COUNT:SEGMENT_COUNT]
-
-
-def train_model(user_id):
-    train_set, train_labels = build_train_set(user_id)
+def train_model(user_id, all_features_of_all_users):
+    all_features_of_id = all_features_of_all_users[user_id]
+    train_set, train_labels = build_train_set(user_id, all_features_of_all_users)
+    test_set = all_features_of_id[TRAIN_SEGMENT_COUNT:SEGMENT_COUNT]
     text_clf = RandomForestClassifier(n_estimators=100)
+
     commends = list(build_word_dict().keys())
     X_train = pd.DataFrame(data=train_set, columns=commends)
     text_clf.fit(X_train, train_labels)
@@ -94,9 +138,8 @@ def train_model(user_id):
     predicted.to_csv("predicted.csv", mode='a')
 
 
-def main():
-    tidf_n_grams()
-    best_ngrams(0, tidf_n_grams())
+if __name__ == "__main__":
+    tidf_grams = tidf_n_grams()
+    all_features_of_all_users = get_all_features_of_all_users(tidf_grams)
+    train_model(0, tidf_grams)
 
-
-main()
